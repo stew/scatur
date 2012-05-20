47fc332522c3fd32541a58a080a08b15aea55621
Mike O'Connor <stew@vireo.org>
2012-05-20 14:21:22 -0400

    dual license with Apache-2.0 and GPL-3
    Signed-off-by: Mike O'Connor <stew@vireo.org>

--- src/test/scala/add.scala
+++ src/test/scala/add.scala
@@ -1,17 +1,10 @@
-//    This software is Copyright 2012, Mike (stew) O'Connor <stew@vireo.org>
+//  This software is Copyright 2012, Mike (stew) O'Connor <stew@vireo.org>
 //
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  This software is dual licensed under the GPL-3 and the Apache 2.0
+//  license.  Feel free to use, modify, and redistribute this software
+//  under the terms of either license.  Both licenses appear verbatim in
+//  the file named COPYING which you should have received as part of this
+//  software.
 
 package turing
 
